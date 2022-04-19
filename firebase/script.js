9a72b931f4c71a3e1a6ec9dd554a56e9c0a4db22
Cole Abbott <caskisail18@icloud.com>
2022-04-18 23:22:10 -0400

    smooth movement

--- firebase/script.js
+++ firebase/script.js
@@ -15,12 +15,49 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+let playerID
+let playerRef
+
 function preload() {
-    const {playerID, playerRef} = connect()
-    console.log(playerID)
+    connect()
+    const allPlayerRef = firebase.database().ref("players");
+    
+    //array of phaser objects for each player, key is player ID
+    allPlayers = {}
+
+
+    allPlayerRef.on("value", (snapshot) =>{
+        //update allPlayers array position
+        data = snapshot.val()
+
+        for (let key in data) {
+            if (key != playerID) {
+                //allPlayers[key].x = data[key].x
+                //allPlayers[key].y = data[key].y
+                move(allPlayers[key], data[key].x, data[key].y)
+            }
+        }
+    })
+
+    allPlayerRef.on("child_added", (snapshot) =>{
+        //add new player
+        const data = snapshot.val()
+
+        if (data.playerID != playerID) {
+            //add new phaser player to allPlayers
+            allPlayers[data.playerID] = this.physics.add.sprite(data.x, data.y, 'player_blue');
+        }
+    })
+
+    allPlayerRef.on("child_removed", (snapshot) =>{
+        removedID = snapshot.val().playerID
+        allPlayers[removedID].destroy()
+    })
+    
     this.load.image("grass_1", "./sprites/grass_1.png")
     this.load.image("road_1", "./sprites/road_1.png")
     this.load.image("player", "./sprites/player.png")
+    this.load.image("player_blue", "./sprites/player_blue.png")
 }
 
 function create() {
@@ -37,12 +74,8 @@ function create() {
 
     //add/config player
     player = this.physics.add.sprite(100, 100, 'player');
-    otherPlayers = this.physics.add.sprite(100, 100, 'player');
     player.setBounce(0.2);
-    player.setDrag(0.6)
-    player.setDamping(true)
     player.setCollideWorldBounds(true);
-    player.showDebugVelocity = true;
 
     cursorKeys = this.input.keyboard.addKeys({
         'up': Phaser.Input.Keyboard.KeyCodes.W,
@@ -52,29 +85,66 @@ function create() {
     });
 }
 
-function update(){
+const speed = 200
 
+const keyActions = {
+    w: {
+        down: (player) => player.setVelocityY(-speed),
+        up: (player) => player.setVelocityY(0),
+    },
+    s: {
+        down: (player) => player.setVelocityY(speed),
+        up: (player) => player.setVelocityY(0),
+    },
+    a: {
+        down: (player) => player.setVelocityX(-speed),
+        up: (player) => player.setVelocityX(0),
+    },
+    d: {
+        down: (player) => player.setVelocityX(speed),
+        up: (player) => player.setVelocityX(0),
+    }
 }
 
+function update(){
+
+    for (let key in cursorKeys) {
+        
+        if(cursorKeys[key].isDown){
+            
+            letter = cursorKeys[key].originalEvent.key
+            keyActions[letter].down(player)
+        }
+        if(Phaser.Input.Keyboard.JustUp(cursorKeys[key])){
+            letter = cursorKeys[key].originalEvent.key
+            keyActions[letter].up(player)
+        }
+    }
+}
 
 function connect() {
     //connect to firebse
-    let playerID, playerRef
     
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             //logged in
-            console.log(user)
-
             playerID = user.uid
             playerRef = firebase.database().ref(`players/${playerID}`)
-
             playerRef.set({
                 playerID,
                 x: 100,
                 y: 100,
                 name: "cole"
             })
+            setInterval(() => {
+                playerRef = firebase.database().ref(`players/${playerID}`)
+                playerRef.set({
+                    playerID,
+                    x: player.x,
+                    y: player.y,
+                    name: "cole"
+                })
+            }, 50)
             //Remove me from Firebase when I diconnect
             playerRef.onDisconnect().remove();
 
@@ -89,6 +159,27 @@ function connect() {
     firebase.auth().signInAnonymously().catch( (error) => {
         console.log(error.code, error.message)
     })
+    return
+}
 
-    return {playerID, playerRef}
+//move sprite to x,y in 50ms
+async function move(player, x, y) {
+    distance = {
+        x: (x - player.x) / 50,
+        y: (y - player.y) / 50,
+    }
+    
+    //move it 1/25th of the disance every 1ms
+    for (let i = 0; i < 50; i++) {
+        player.x += distance.x
+        player.y += distance.y
+        await delay(1)
+    }
+    //make sure it is in right position
+    //player.x = x
+    //player.y = y
+}
+
+function delay(ms) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
 }
