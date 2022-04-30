635135b6892ffb372355c662f206012d3492f3a6
Cole Abbott <caskisail18@icloud.com>
2022-04-30 10:10:17 -0400

    map+camera

--- firebase/script.js
+++ firebase/script.js
@@ -1,6 +1,6 @@
-
 var config = {
     type: Phaser.AUTO,
+    pixelArt: true,
     width: 800,
     height: 600,
     physics: {
@@ -21,17 +21,21 @@ let playerRef
 function preload() {
     connect()
     const allPlayerRef = firebase.database().ref("players");
-    
-    //array of phaser objects for each player, key is player ID
-    allPlayers = {}
 
+    //object of phaser objects for each player, key is player ID
+    allPlayers = {}
+    
     //runs whenever players is updated
     allPlayerRef.on("value", (snapshot) =>{
         data = snapshot.val()
         //moves all players to new position
         for (let key in data) {
             if (key != playerID) {
-                move(allPlayers[key], data[key].x, data[key].y)
+                //set velocity to move to setpoint and stop after 25ms
+                this.physics.moveTo(allPlayers[key], data[key].x, data[key].y, 25, 25)
+                setTimeout(() => {
+                    allPlayers[key].setVelocity(0)
+                }, 25)
             }
         }
     })
@@ -52,29 +56,71 @@ function preload() {
         allPlayers[removedID].destroy()
     })
     
+    //object of non player entities in game, one for child entities, one for others
+    entities = {}
+    childEntities = {}
+
+    //db ref for entities
+    const entitiesRef = firebase.database().ref("entities");
+
+    //runs whenever entity is updated
+    entitiesRef.on("value", (snapshot) =>{
+        data = snapshot.val()
+        //moves all entitiesRef to new position
+        for (let key in data) {
+            if (data[key].parent != playerID) {
+                //set velocity to move to setpoint and stop after 25ms
+                this.physics.moveTo(entities[key], data[key].x, data[key].y, 25, 25)
+                setTimeout(() => {
+                    entities[key].setVelocity(0)
+                }, 25)
+            }
+        }
+    })
+
+    //runs whenever a new entity is added
+    entitiesRef.on("child_added", (snapshot) =>{
+        const data = snapshot.val()
+        //check if you are parent
+        if (data.parent != playerID) {
+            //add new phaser sprite to entities
+            entities[data.entityID] = this.physics.add.sprite(data.x, data.y, 'bullet');
+        }
+    })
+
+    //runs whenever a entity is destroyed, deletes entity from game
+    entitiesRef.on("child_removed", (snapshot) =>{
+        data = snapshot.val()
+        if (data.parent != playerID) {
+            entities[data.entityID].destroy()
+        }
+    })
+
+
     //load sprites
-    this.load.image("grass_1", "./sprites/grass_1.png")
-    this.load.image("road_1", "./sprites/road_1.png")
+    this.load.image("fountain", "./sprites/fountain.png")
+    this.load.image("pathDown_1", "./sprites/pathDown_1.png")
     this.load.image("player", "./sprites/player.png")
     this.load.image("player_blue", "./sprites/player_blue.png")
+    this.load.image("bullet","./sprites/bullet.png" )
 }
 
 function create() {
     //add background
-    this.add.image(0, 0, 'grass_1');
-    this.add.image(128, 0, 'grass_1');
-    this.add.image(256, 0, 'grass_1');
-    this.add.image(384, 0, 'road_1');
-    this.add.image(384, 128, 'road_1');
-    this.add.image(384, 256, 'road_1');
-    this.add.image(384, 384, 'road_1');
-    this.add.image(384, 512, 'road_1');
+    
+    this.add.image(1000, 1000, 'fountain').setScale(4)
+    this.add.image(1000, 1512, 'pathDown_1').setScale(4)
 
+    this.physics.world.setBounds(0, 0, 2000, 2000);
 
     //add/config player
-    player = this.physics.add.sprite(100, 100, 'player');
+    player = this.physics.add.sprite(1000, 1000, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
+    this.cameras.main.setBounds(0, 0, 2000, 2000);
+    this.cameras.main.startFollow(player)
+
 
     movementKeys = this.input.keyboard.addKeys({
         'up': Phaser.Input.Keyboard.KeyCodes.W,
@@ -106,15 +152,10 @@ const keyActions = {
     d: {
         down: (player) => player.setVelocityX(speed),
         up: (player) => player.setVelocityX(0),
-    },
-    ' ': {
-        down: () => console.log("down"),
-        up: () => console.log("up"),
     }
 }
 
 function update(){
-    
     //parses keys
     for (let key in movementKeys) {
         if(movementKeys[key].isDown){
@@ -133,7 +174,6 @@ function update(){
     }
 }
 
-
 function connect() {
     //connect to firebse
     firebase.auth().onAuthStateChanged((user) => {
@@ -150,13 +190,28 @@ function connect() {
             })
             //send position every 50ms
             setInterval(() => {
+                //send player data
                 playerRef = firebase.database().ref(`players/${playerID}`)
+                entitiesRef = firebase.database().ref(`entities/${playerID}`);
+
                 playerRef.set({
                     playerID,
                     x: player.x,
                     y: player.y,
                     name: "cole"
                 })
+
+                
+                //send entity data for child entities
+                for (let entityID in childEntities) {
+                    dbRef =  firebase.database().ref(`entities/${entityID}`);
+                    dbRef.set({
+                        entityID,
+                        parent: playerID,
+                        x: childEntities[entityID].x,
+                        y: childEntities[entityID].y
+                    })
+                }
             }, 50)
             //Remove me from Firebase when I diconnect
             playerRef.onDisconnect().remove();
@@ -168,37 +223,40 @@ function connect() {
         
     })
 
-
     firebase.auth().signInAnonymously().catch( (error) => {
         console.log(error.code, error.message)
     })
     return
 }
 
-//move sprite to x,y in 50ms
-async function move(player, x, y) {
-    distance = {
-        x: (x - player.x) / 50,
-        y: (y - player.y) / 50,
-    }
-    
-    //move it 1/25th of the disance every 1ms
-    for (let i = 0; i < 50; i++) {
-        player.x += distance.x
-        player.y += distance.y
-        await delay(1)
-    }
-    //make sure it is in right position
-    //player.x = x
-    //player.y = y
-}
-
 function shoot(player, game) {
-    const entities = game.physics.add.sprite(player.x, player.y, 'player')
+    //make entity ID
+    const entityID = Math.random().toString(36).substring(2, 16)
 
-}
+    //connect to firebase
+    const dbRef = firebase.database().ref(`entities/${entityID}`);
 
+    //create sprite
+    entity = game.physics.add.sprite(player.x, player.y, 'bullet'),
+    entity.setCollideWorldBounds(true)
+    entity.setBounce(1)
 
-function delay(ms) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    childEntities[entityID] = entity
+    //add entity to childEntities
+
+    //add entity to firebase 
+    dbRef.set({
+        x: entity.x,
+        y: entity.y,
+        parent: playerID,
+        entityID
+    })
+    entity.setVelocity(player.body.velocity.x * 1.5, player.body.velocity.y * 1.5)
+    
+    //delete bullet after 2 seconds
+    setTimeout(() => {
+        dbRef.remove();
+        childEntities[entityID].destroy()
+        delete childEntities[entityID]
+    }, 2000);
 }
